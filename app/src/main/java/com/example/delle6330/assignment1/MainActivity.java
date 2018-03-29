39dfd9ab7227a48e02a1010a4ddfa7776ec9644a
Svetlana <netc0004@algonquinlive.com>
2018-03-28 22:40:30 -0400

    Movies

--- app/src/main/java/com/example/delle6330/assignment1/MainActivity.java
+++ app/src/main/java/com/example/delle6330/assignment1/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.delle6330.assignment1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.button4:
+                Intent intent = new Intent(this, MovieActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
