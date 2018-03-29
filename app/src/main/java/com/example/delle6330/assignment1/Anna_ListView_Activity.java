5545c8458cef623985ce27b1bd8c9a2e95ed96f1
Pavel Jilinski <jili0001@algonquinlive.com>
2018-03-29 11:23:38 -0400

    initial commit

--- app/src/main/java/com/example/delle6330/assignment1/Anna_ListView_Activity.java
+++ app/src/main/java/com/example/delle6330/assignment1/Anna_ListView_Activity.java
@@ -1,14 +1,9 @@
 package com.example.delle6330.assignment1;
 
-import android.content.Intent;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-import static com.example.delle6330.assignment1.R.id.questionViewAnna;
 
 public class Anna_ListView_Activity extends AppCompatActivity {
 
@@ -18,10 +13,8 @@ public class Anna_ListView_Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_anna__list_view_);
+        setContentView(R.layout.activity_anna_list_view);
         answerAnna = (Button) findViewById(R.id.answerButtonAnna);
-//        listViewAnna = (ListView) findViewById(R.id.questionViewAnna);
-
         answerAnna.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
