76dc1102dcddc961aea8b21358afa336a4f3bb26
Anna Shteyngart <shte0006@algonquinlive.com>
2018-04-08 00:14:38 -0400

    commit ms2

---
+++ app/src/main/java/com/example/delle6330/assignment1/AnnaTrueFalseFragment.java
@@ -0,0 +1,22 @@
+package com.example.delle6330.assignment1;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class AnnaTrueFalseFragment extends Fragment {
+
+    public AnnaTrueFalseFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_anna_true_false, container, false);
+    }
+}
